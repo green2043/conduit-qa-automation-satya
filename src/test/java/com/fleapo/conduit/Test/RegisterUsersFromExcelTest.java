5a14cac9f453227dc1a518a1dbc885542de0f3ea
Satya <satyasahoo2043@gmail.com>
2025-12-02 12:34:07 +0530

    code fix

--- src/test/java/com/fleapo/conduit/Test/RegisterUsersFromExcelTest.java
+++ src/test/java/com/fleapo/conduit/Test/RegisterUsersFromExcelTest.java
@@ -3,6 +3,7 @@ package com.fleapo.conduit.Test;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import com.fleapo.conduit.Base.BaseTest;
 import com.fleapo.conduit.core.util.ExcelUtil;
@@ -21,16 +22,42 @@ public class RegisterUsersFromExcelTest extends BaseTest {
     @Test(dataProvider = "registerExcelData")
     public void registerUsersUsingExcel(String username, String email, String password) {
 
+        SoftAssert soft = new SoftAssert();
+
         HomePage home = new HomePage(driver);
         RegisterPage rp = home.goToRegisterPage();
 
         HomePage homeAfterRegister = rp.doRegister(username, email, password);
 
-        Assert.assertNotNull(homeAfterRegister,
-                "Registration failed for user from Excel: " + username);
+        if (homeAfterRegister != null) {
+
+            
+            System.out.println("Registration successful for: " + username);
+
+            
+            soft.assertNotNull(
+                    homeAfterRegister,
+                    "Registration should succeed for new user: " + username
+            );
+
+            SettingsPage sp = homeAfterRegister.goToSettings();
+            sp.doLogout();
+
+        } else {
+
+            
+            String error = rp.getAnyErrorMessage();
+            System.out.println("User already exists or invalid data. Error: " + error);
+
+            
+            Assert.assertTrue(
+                    error.contains("has already been taken"),
+                    "Expected duplicate user error message not found for: " + username
+            );
+        }
 
         
-        SettingsPage sp = homeAfterRegister.goToSettings();
-        sp.doLogout();
+        soft.assertAll();
     }
+
 }
